fae1da6db9d66a83e22d112e0b4795a692396fd7
zeMirco <mirco.zeiss@gmail.com>
2014-02-02 13:51:43 +0100

    add grunt for css and html minify

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    cssmin: {
+      combine: {
+        files: {
+          'public/css/styles.min.css': [
+            '_public/css/syntax.css', 
+            '_public/css/poole.css', 
+            '_public/css/lanyon.css', 
+            '_public/css/custom.css', 
+          ]
+        }
+      }
+    },
+
+    htmlmin: {
+      prod: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true,
+          collapseBooleanAttributes: true,
+          removeAttributeQuotes: true,
+          removeRedundantAttributes: true,
+          removeEmptyAttributes: true,
+          removeOptionalTags: true,
+          removeEmptyElements: true
+        },
+        files: [
+          {
+            expand: true,     // Enable dynamic expansion.
+            cwd: '_site/',      // Src matches are relative to this path.
+            src: ['**/*.html'], // Actual pattern(s) to match.
+            dest: '_site/'
+          }
+        ]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+  // Default task(s).
+  grunt.registerTask('default', ['cssmin', 'htmlmin']);
+
+};
