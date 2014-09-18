144b3f5fd0174b25234dfe5e629eafcd1ab35806
zeMirco <mirco.zeiss@gmail.com>
2014-09-18 16:35:31 +0200

    add angular test page for testing render with google

---
+++ angular/app.js
@@ -0,0 +1,20 @@
+
+angular
+.module('myApp', [])
+.config([function() {}])
+.controller('IndexCtrl', ['$scope', '$http', function($scope, $http) {
+  console.log('awesome');
+
+  $scope.countries = [
+    'Germany',
+    'Italy',
+    'Spain',
+    'Netherlands',
+    'France'
+  ]
+
+  $http.get('http://104.131.24.97:49159/json').success(function(data) {
+    $scope.animals = data;
+  });
+
+}]);
